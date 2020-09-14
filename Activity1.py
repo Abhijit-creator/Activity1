1f78ab663eebcb5eb7624e85433cb80322d5519e
Abhijit-creator <70905251+Abhijit-creator@users.noreply.github.com>
2020-09-15 01:52:30 +0530

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+for x in range(2000 , 3200):
+    if (x%7==0) and (x%5!=0):
+        print(x,end=",")
+
+
+# In[39]:
+
+
+first_name = input("fname: ")
+last_name = input("lname: ")
+print(last_name + " " + first_name)
+
+
+# In[40]:
+
+
+p = 22/7
+d = int(input(" "))
+r = d/2
+v = 4/3*p*r**3
+print(v)
+
+
+# In[ ]:
+
+
+
+
